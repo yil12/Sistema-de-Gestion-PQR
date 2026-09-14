2e69095fe4091a2eb3249a491ffc990505e4b94b
yil12 <yilbersilo@gmail.com>
2026-09-14 17:06:24 -0500

    [SDGP-81] feat: gestion de solicitantes

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ from app.api.routes.seguimiento import router as seguimiento_router
 from app.api.routes.asignacion import router as asignacion_router
 from app.api.routes.escalamiento import router as escalamiento_router
 from app.api.routes.resolucion import router as resolucion_router
+from app.api.routes.solicitante import router as solicitante_router
+
 
 
 
@@ -39,6 +41,8 @@ app.include_router(seguimiento_router)
 app.include_router(asignacion_router)
 app.include_router(escalamiento_router)
 app.include_router(resolucion_router)
+app.include_router(solicitante_router)
+
 
 
 
--- app/repositories/solicitante_repository.py
+++ app/repositories/solicitante_repository.py
@@ -3,8 +3,45 @@ from sqlalchemy.orm import Session
 from app.models.solicitante import Solicitante
 
 
+def create_solicitante(
+    db: Session,
+    solicitante: Solicitante,
+) -> Solicitante:
+    db.add(solicitante)
+    db.flush()
+    db.refresh(solicitante)
+
+    return solicitante
+
+
 def get_solicitante_by_id(
     db: Session,
     solicitante_id: int,
 ) -> Solicitante | None:
     return db.get(Solicitante, solicitante_id)
+
+
+def get_solicitante_by_documento(
+    db: Session,
+    numero_documento: str,
+) -> Solicitante | None:
+    return (
+        db.query(Solicitante)
+        .filter(
+            Solicitante.numero_documento == numero_documento
+        )
+        .first()
+    )
+
+
+def get_solicitante_by_email(
+    db: Session,
+    email: str,
+) -> Solicitante | None:
+    return (
+        db.query(Solicitante)
+        .filter(
+            Solicitante.email == email
+        )
+        .first()
+    )
