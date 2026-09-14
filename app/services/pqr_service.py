c5552aa922b64f25bee2c473280ac6a5d23d5ef6
yil12 <yilbersilo@gmail.com>
2026-09-13 20:58:20 -0500

    [SDGP-53] feat: implementar service para consulta de PQR

--- app/services/pqr_service.py
+++ app/services/pqr_service.py
@@ -1,13 +1,19 @@
 from sqlalchemy.orm import Session
 
 from app.models.pqr import PQR
-from app.repositories.pqr_repository import create_pqr
 from app.schemas.pqr import PQRCreate
 from app.utils.radicado import generar_radicado
 
 from app.repositories.solicitante_repository import (
     get_solicitante_by_id,
 )
+
+from app.repositories.pqr_repository import (
+    create_pqr,
+    get_pqr_by_id,
+    get_pqr_by_radicado,
+)
+
 from app.core.exceptions import BusinessException
 
 
@@ -47,3 +53,32 @@ def register_pqr(db: Session, data: PQRCreate) -> PQR:
         db.rollback()
         raise
 
+
+def get_pqr_by_id_service(
+    db: Session,
+    pqr_id: int,
+) -> PQR:
+    pqr = get_pqr_by_id(db, pqr_id)
+
+    if pqr is None:
+        raise BusinessException(
+            status_code=404,
+            detail="La PQR indicada no existe.",
+        )
+
+    return pqr
+
+
+def get_pqr_by_radicado_service(
+    db: Session,
+    radicado: str,
+) -> PQR:
+    pqr = get_pqr_by_radicado(db, radicado)
+
+    if pqr is None:
+        raise BusinessException(
+            status_code=404,
+            detail="No existe una PQR asociada al radicado indicado.",
+        )
+
+    return pqr
