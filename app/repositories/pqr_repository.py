c8df93423ef68347354cc74f61e956a589e0b54a
yil12 <yilbersilo@gmail.com>
2026-09-13 20:53:13 -0500

    [SDGP-52] feat: implementar repository para consulta de PQR

--- app/repositories/pqr_repository.py
+++ app/repositories/pqr_repository.py
@@ -9,3 +9,21 @@ def create_pqr(db: Session, pqr: PQR) -> PQR:
     db.refresh(pqr)
 
     return pqr
+
+
+def get_pqr_by_id(
+    db: Session,
+    pqr_id: int,
+) -> PQR | None:
+    return db.get(PQR, pqr_id)
+
+
+def get_pqr_by_radicado(
+    db: Session,
+    radicado: str,
+) -> PQR | None:
+    return (
+        db.query(PQR)
+        .filter(PQR.radicado == radicado)
+        .first()
+    )
