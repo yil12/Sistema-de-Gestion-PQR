eb957b5f310491a130323a0f645dbc4b10193f1e
yil12 <yilbersilo@gmail.com>
2026-09-17 18:59:14 -0500

    agregando CORS de url render

--- app/main.py
+++ app/main.py
@@ -39,6 +39,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://sistema-de-gestion-pqr-front-1.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
